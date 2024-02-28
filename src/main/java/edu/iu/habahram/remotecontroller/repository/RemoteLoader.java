13cda7ed8e68aaee3430999814c23714a42455ca
agawas <agawas@iu.edu>
2024-02-28 09:03:06 -0500

    done

--- src/main/java/edu/iu/habahram/remotecontroller/repository/RemoteLoader.java
+++ src/main/java/edu/iu/habahram/remotecontroller/repository/RemoteLoader.java
@@ -4,21 +4,40 @@ import edu.iu.habahram.remotecontroller.model.DeviceData;
 import edu.iu.habahram.remotecontroller.model.Light;
 import edu.iu.habahram.remotecontroller.model.RemoteControl;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class RemoteLoader implements  IRemoteLoader{
-    HashMap<Integer, RemoteControl> remoteControls = new HashMap<>();
+public class RemoteLoader implements IRemoteLoader {
+
+    private static volatile RemoteLoader instance;
+
+    private HashMap<Integer, RemoteControl> remoteControls = new HashMap<>();
+
+    public RemoteLoader() {
+        // Private constructor to prevent instantiation.
+    }
+
+    public static RemoteLoader getInstance() {
+        if (instance == null) {
+            synchronized (RemoteLoader.class) {
+                if (instance == null) {
+                    instance = new RemoteLoader();
+                }
+            }
+        }
+        return instance;
+    }
+
     @Override
     public void setup(int id, List<DeviceData> devices) {
         RemoteControl remoteControl = new RemoteControl(devices.size());
-        for(DeviceData device : devices) {
+        for (DeviceData device : devices) {
             switch (device.type()) {
                 case "light":
                     Light light = new Light(device.location());
                     remoteControl.setCommand(device.slot(), light::on, light::off);
                     break;
+                // Add more cases for other device types if needed
             }
         }
         remoteControls.put(id, remoteControl);
@@ -27,12 +46,11 @@ public class RemoteLoader implements  IRemoteLoader{
 
     @Override
     public String onButtonWasPushed(int id, int slot) {
-         return remoteControls.get(id).onButtonWasPushed(slot);
+        return remoteControls.get(id).onButtonWasPushed(slot);
     }
 
     @Override
     public String offButtonWasPushed(int id, int slot) {
         return remoteControls.get(id).offButtonWasPushed(slot);
-
     }
 }
